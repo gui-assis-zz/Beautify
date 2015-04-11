e99f0b1d1fde032b77a7be48c443a25f7d4549c1
gui-assis <Guilherme@Mac.local>
2015-04-11 15:06:31 -0300

    Added tab's view controllers do project

---
+++ Beautify/View Controllers/FavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesViewController.h
+//  Beautify
+//
+//  Created by Guilherme Augusto on 11/04/15.
+//  Copyright (c) 2015 Cousins Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesViewController : UIViewController
+
+@end
---
+++ Beautify/View Controllers/ProfessionalListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfessionalListViewController.h
+//  Beautify
+//
+//  Created by Guilherme Augusto on 11/04/15.
+//  Copyright (c) 2015 Cousins Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfessionalListViewController : UIViewController
+
+@end
