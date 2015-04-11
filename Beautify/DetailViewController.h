9649de3899edfa6fe930fe5395271be308e8a942
gui-assis <Guilherme@Mac.local>
2015-04-11 13:36:13 -0300

    Initial Commit

---
+++ Beautify/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Beautify
+//
+//  Created by Guilherme Augusto on 11/04/15.
+//  Copyright (c) 2015 Cousins Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
