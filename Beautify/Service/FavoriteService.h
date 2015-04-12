e8863c74dda99f4d7d70bed061cf272e34b75347
gui-assis <Guilherme@Mac.local>
2015-04-12 02:39:19 -0300

    Favorite Service

---
+++ Beautify/Service/FavoriteService.h
@@ -0,0 +1,14 @@
+//
+//  FavoriteService.h
+//  Beautify
+//
+//  Created by Guilherme Augusto on 12/04/15.
+//  Copyright (c) 2015 Cousins Corp. All rights reserved.
+//
+
+#import "BaseService.h"
+
+@interface FavoriteService : BaseService
+
+
+@end
---
+++ Beautify/Service/FavoriteService.m
@@ -0,0 +1,13 @@
+//
+//  FavoriteService.m
+//  Beautify
+//
+//  Created by Guilherme Augusto on 12/04/15.
+//  Copyright (c) 2015 Cousins Corp. All rights reserved.
+//
+
+#import "FavoriteService.h"
+
+@implementation FavoriteService
+
+@end
